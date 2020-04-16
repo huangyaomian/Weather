c6c8310df7fd58af53ad59ec19d2613646f9801a
mika <345249887@qq.com>
2020-04-16 08:25:41 +0800

    1

--- app/src/main/java/com/hym/weather/CityManagerActivity.java
+++ app/src/main/java/com/hym/weather/CityManagerActivity.java
@@ -3,12 +3,49 @@ package com.hym.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-public class CityManagerActivity extends AppCompatActivity {
+import com.hym.weather.db.DatabaseBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityManagerActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView addIv,backIv,deleteIv;
+    ListView cityLv;
+    List<DatabaseBean> mDatas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_manager);
+        addIv = findViewById(R.id.city_iv_add);
+        backIv = findViewById(R.id.city_iv_back);
+        deleteIv = findViewById(R.id.city_iv_delete);
+        cityLv = findViewById(R.id.city_Iv);
+        mDatas = new ArrayList<>();
+        //设置点击监听事件
+        addIv.setOnClickListener(this);
+        backIv.setOnClickListener(this);
+        deleteIv.setOnClickListener(this);
+        //设置适配器
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.city_iv_add:
+
+                break;
+            case R.id.city_iv_back:
+
+                break;
+            case R.id.city_iv_delete
+
+                break;
+
+        }
+
     }
 }
